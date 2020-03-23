33465d532b04569173e37a3ff38070f4e4fa8f0a
wangk-sy <1638869567@qq.com>
2020-03-23 09:39:57 +0800

    No.2

--- src/main/java/com/company/project/model/Material.java
+++ src/main/java/com/company/project/model/Material.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+import java.util.List;
+
 /**
  * @ClassName :material
  * @Description :TODO
@@ -15,5 +17,13 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class Material {
-
+    private Integer id;
+    private String name;
+    private Integer count=0;
+    private String classNumber;
+    private List<License> content;
+    private String isScan;
+    private String refer;
+    private String flag;
+    private Integer shareCount;
 }
